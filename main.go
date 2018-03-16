379f658307ca549dddf56cc620239fac8abae5f1
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2018-03-15 22:38:28 -0500

    got the Get request and parsed JSON correctly

--- main.go
+++ main.go
@@ -5,11 +5,16 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strings"
 )
 
-type Game struct {
-	Name, Deck, OriginalReleaseDate string
+type Results struct {
+	Deck                string
+	Name                string
+	OriginalReleaseDate string `json:"original_release_date"`
+}
+
+type GiantBombSearchResponse struct {
+	Results []Results
 }
 
 // This function must be named ServeHTTP() and these argument methods must be in this order and *http.Request must be a reference
@@ -39,10 +44,11 @@ func main() {
 	// 		panic(err)
 	// 	}
 
-	content := giantBombAPI("http://www.giantbomb.com/api/game/3030-4725/?api_key=f10f942d8c6c6a04af3c3774e257daa795c10589&format=json")
+	results, _ := search("http://www.giantbomb.com/api/game/3030-4725/?api_key=f10f942d8c6c6a04af3c3774e257daa795c10589&format=json")
+	fmt.Printf("Results : %+v", results)
 	// fmt.Print(content)
-	games := gamesFromJson(content)
-	fmt.Print(games)
+	// games := gamesFromJSON(results)
+	// fmt.Print(games)
 
 }
 
@@ -50,47 +56,65 @@ func main() {
 
 // }
 
-func giantBombAPI(url string) string {
+func giantBombAPI(url string) ([]byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		// return []byte{}, err
-		panic(err)
+		return []byte{}, err
 	}
 
-	fmt.Printf("Giantbomb response type: %T\n", resp)
+	// fmt.Printf("Giantbomb response type: %T\n", resp)
 
 	defer resp.Body.Close()
-	bytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		// return []byte{}, err
-		panic(err)
-	}
+	// bytes, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	// return []byte{}, err
+	// 	panic(err)
+	// }
 
-	return string(bytes)
-	// return ioutil.ReadAll(resp.Body)
+	// return string(bytes)
+	return ioutil.ReadAll(resp.Body)
+
 }
 
-func gamesFromJson(content string) []Game {
-	games := make([]Game, 0, 20)
-	// use the a json decoder to decode a string object that is read by the string reader
-	decoder := json.NewDecoder(strings.NewReader(content))
-	// This removes the array brackets that wrap the json results
-	// This appear to only be visible if there is more than one json object
-	// Will this error if there is no array brackets to remove?
-	_, err := decoder.Token()
+// You are getting an empty slice when []Results, but if it is not a slice you get the correct outcome.
+func search(query string) ([]Results, error) {
+	var g GiantBombSearchResponse
+	body, err := giantBombAPI(query)
 	if err != nil {
-		// return []byte{}, err
-		panic(err)
+		return []Results{}, err
 	}
-	var game Game
-	for decoder.More() {
-		// This parses the json and pull only the fields that match the ones in my stuct
-		err := decoder.Decode(&game)
-		if err != nil {
-			// return []byte{}, err
-			panic(err)
-		}
-		games = append(games, game)
-	}
-	return games
+	err = json.Unmarshal(body, &g)
+	return g.Results, err
+
 }
+
+// func gamesFromJSON(content string) []Game {
+// 	games := make([]Game, 0, 20)
+// 	// use the a json decoder to decode a string object that is read by the string reader
+// 	fmt.Print("From fameFromJson: \n", content)
+// 	decoder := json.NewDecoder(strings.NewReader(content))
+// 	// This removes the array brackets that wrap the json results
+// 	// This appear to only be visible if there is more than one json object
+// 	// Will this error if there is no array brackets to remove?
+// 	// _, err := decoder.Token()
+// 	// if err != nil {
+// 	// 	// return []byte{}, err
+// 	// 	panic(err)
+// 	// }
+// 	var game Game
+// 	fmt.Print("The decode prior to decoder.More() \n", decoder)
+// 	for decoder.More() {
+// 		// This parses the json and pull only the fields that match the ones in my stuct
+// 		err := decoder.Decode(&game)
+// 		if err != nil {
+// 			// return []byte{}, err
+// 			panic(err)
+// 		}
+// 		games = append(games, game)
+// 	}
+// 	return games
+// }
+
+// func anotherGamesJSON(content string) []Game {
+
+// }
